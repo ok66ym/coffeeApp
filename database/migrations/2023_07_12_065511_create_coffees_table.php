f882c7e036874e891c8c63e6c9aeca4d78b5b713
ok66ym <yu.6.425@icloud.com>
2023-07-20 04:52:12 +0900

    Changed the evaluation items(bitter, acidity, etc) of the coffees tables to nullable.

--- database/migrations/2023_07_12_065511_create_coffees_table.php
+++ database/migrations/2023_07_12_065511_create_coffees_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
     {
         Schema::create('coffees', function (Blueprint $table) {
             $table->id();
-            $table->double('bitter', 3, 2);                     //苦味．3桁で小数点以下2桁の少数カラム
-            $table->double('acidity', 3, 2);                    //酸味．同上
-            $table->double('rich', 3, 2);                       //コク．同上
-            $table->double('sweet', 3, 2);                      //甘味．同上
-            $table->double('smell', 3, 2);                      //香り．同上
+            $table->double('bitter', 3, 2)->nullable();         //苦味．3桁で小数点以下2桁の少数カラム
+            $table->double('acidity', 3, 2)->nullable();        //酸味．同上
+            $table->double('rich', 3, 2)->nullable();           //コク．同上
+            $table->double('sweet', 3, 2)->nullable();          //甘味．同上
+            $table->double('smell', 3, 2)->nullable();          //香り．同上
             $table->string('roasted', 50)->nullable();          //焙煎度．50文字以内
             $table->string('name', 100);                        //珈琲豆の名前．100文字以内
             $table->string('area_name', 100)->nullable();       //産地名．100文字以内
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->string('image')->nullable();                //画像
             $table->string('shop_name', 100);                   //販売店名．100文字以内
             $table->string('shop_url', 200);                    //珈琲豆の販売サイト．100文字以内
-            $table->string('official_url', 100);                //販売店の公式URL
+            $table->string('official_url', 100)->nullable();    //販売店の公式URL
             $table->string('map')->nullable();                  //google map
             $table->timestamps();
         });
